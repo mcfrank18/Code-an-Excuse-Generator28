2c641f8d2c4c6b9894b582da87cfde5795a2056c
mcfrank18 <fmperezg2010@gmail.com>
2022-07-20 22:51:21 +0000

    excuse tarea

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  function excuseGenerator(who, what, when) {
+    let numwho = Math.floor(Math.random() * who.length);
+    let numwhat = Math.floor(Math.random() * what.length);
+    let numwhen = Math.floor(Math.random() * when.length);
+    return who[numwho] + " " + what[numwhat] + " " + when[numwhen];
+  }
+
+  document.querySelector("#excuse").innerHTML = excuseGenerator(
+    who,
+    what,
+    when
+  );
 };
